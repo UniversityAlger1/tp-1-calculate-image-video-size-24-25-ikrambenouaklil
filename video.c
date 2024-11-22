ca31aab1be6639b923b8074633a7c58f431d8724
ikram <ikrambn2002@gmail.com>
2024-11-22 14:51:53 +0100

    video

--- video.c
+++ video.c
@@ -10,6 +10,20 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+float result = fps *(w*h*durationCredits + 3*w*h*durationMovie); // byte 
+if (strcmp(unit, "bt") == 0 ){
+   return result * 8 ; //bits 
+ } 
+  if (strcmp(unit, "ko") == 0){
+   return result / 1024 ; //kilobits 
+ } 
+  if (strcmp(unit, "mo") == 0){
+   return result / (1024*1024) ; //megabits
+ }
+ if (strcmp(unit, "go") == 0){
+   return result / (1024*1024*1024) ; //gigabits 
+ }
+
 
    // YOUR CODE HERE - END
    return 0;
